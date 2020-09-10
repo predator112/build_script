01a6d85d4d4396f0d30126baf6248311d6e86568
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-09-10 20:25:12 +0700

    Revert "Change to GCC 10"
    This reverts commit 04716e5482acd435e67d8c6787bb23c2de93af94.

--- circle.sh
+++ circle.sh
@@ -102,7 +102,7 @@ cloneak3() {
 # Make the kernel
 makekernel() {
     kernelstringfix
-    export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-"
+    export CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-linux-gnu-"
     export CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
     make O=out ARCH=arm64 ${DEFCONFIG}
     make -j$(nproc --all) O=out ARCH=arm64
