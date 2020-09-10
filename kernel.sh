21894f2e8f42b49951d181025410ae607a6da42f
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-09-10 13:12:52 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -19,6 +19,15 @@
 
 #Kernel building script
 
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+    exit 1
+}
 
 ##------------------------------------------------------##
 ##----------Basic Informations, COMPULSORY--------------##
@@ -27,29 +36,24 @@
 KERNEL_DIR=$PWD
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="SiLonT"
+ZIPNAME="STRIX"
+
+# Kernel zip name type
+TYPE="nightly"
 
 # The name of the device for which the kernel is built
-MODEL="Redmi Note 8"
+MODEL="Redmi Note 6 Pro"
 
 # The codename of the device
-DEVICE="ginkgo"
-
-# Anykernel
-AK3=$KERNEL_DIR/AnyKernel2
+DEVICE="tulip"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=vendor/ginkgo-perf_defconfig
+DEFCONFIG=tulip_defconfig
 
 # Specify compiler. 
 # 'clang' or 'gcc'
-COMPILER=clang
-	if [ $COMPILER = "gcc" ]
-	then
-		# install few necessary packages
-		apt-get -y install llvm lld
-	fi
+COMPILER=gcc
 
 # Clean source prior building. 1 is NO(default) | 0 is YES
 INCREMENTAL=1
@@ -59,8 +63,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-1001156668998"
-		GRPID="-1001468720637"
+		CHATID="-1001466536460"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
@@ -72,17 +75,15 @@ BUILD_DTBO=0
 
 # Sign the zipfile
 # 1 is YES | 0 is NO
-SIGN=1
-	if [ $SIGN = 1 ]
-	then
-			#Check java installed or not
-			JAVAEXEC="/usr/bin/java"
-			if [ ! -d $JAVAEXEC ]
-			then
-				# install java 8 via aptitude
-				apt-get -y install openjdk-8-jdk;
-			fi
-	fi
+SIGN=0
+
+# Silence the compilation
+# 1 is YES(default) | 0 is NO
+SILENCE=0
+
+# Debug purpose. Send logs on every successfull builds
+# 1 is YES | 0 is NO(default)
+LOG_DEBUG=0
 
 ##------------------------------------------------------##
 ##---------Do Not Touch Anything Beyond This------------##
@@ -91,9 +92,11 @@ SIGN=1
 # set KBUILD_BUILD_VERSION and KBUILD_BUILD_HOST and CI_BRANCH
 
 ## Set defaults first
-KBUILD_BUILD_HOST=$(uname -a | awk '{print $2}')
+DISTRO=$(cat /etc/issue)
+KBUILD_BUILD_HOST=DroneCI
 CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export KBUILD_BUILD_HOST CI_BRANCH
+export token="1206672611:AAGYbqxf4SN8f_Zsg3pa6nxOltilb3e8IN0"
 
 ## Check for CI
 if [ -n "$CI" ]
@@ -123,7 +126,6 @@ COMMIT_HEAD=$(git log --oneline -1)
 
 # Set Date 
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
-BUILD_DATE=$(date +"%Y-%m-%d"-%H%M)
 
 #Now Its time for other stuffs like cloning, exporting, etc
 
@@ -131,29 +133,30 @@ BUILD_DATE=$(date +"%Y-%m-%d"-%H%M)
 	echo " "
 	if [ $COMPILER = "clang" ]
 	then
-		echo "★★Cloning Azure Clang 11"
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang clang-llvm
+		msg "|| Cloning Clang-11 ||"
+		git clone --depth=1 https://github.com/Panchajanya1999/azure-clang.git clang-llvm
+
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	elif [ $COMPILER = "gcc" ]
 	then
+		msg "|| Cloning GCC 9.3.0 baremetal ||"
 		git clone --depth=1 https://github.com/arter97/arm64-gcc.git gcc64
 		git clone --depth=1 https://github.com/arter97/arm32-gcc.git gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 	fi
 
-	echo "★★Toolchains Done, Now Its time for AnyKernel .."
-	git clone https://github.com/azrim/kerneltemplate.git -b dtb "$AK3"
-	echo "★★Cloning libufdt"
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 https://github.com/fiqri19102002/AnyKernel3.git -b lineage-17.1-tulip
+	msg "|| Cloning libufdt ||"
 	git clone https://android.googlesource.com/platform/system/libufdt "$KERNEL_DIR"/scripts/ufdt/libufdt
-	echo "★★Cloning Kinda Done..!!!"
 }
 
 ##------------------------------------------------------##
 
 exports() {
-	export KBUILD_BUILD_USER="azrim"
+	export KBUILD_BUILD_USER="FiqriArdyansyah"
 	export ARCH=arm64
 	export SUBARCH=arm64
 
@@ -184,16 +187,18 @@ tg_post_msg() {
 
 }
 
-
 ##----------------------------------------------------------------##
 
 tg_post_build() {
+	#Post MD5Checksum alongwith for easeness
+	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
 	#Show the Checksum alongwith caption
 	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
 	-F chat_id="$2"  \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
-	-F caption="$3"  
+	-F caption="$3 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"  
 }
 
 ##----------------------------------------------------------##
@@ -201,79 +206,89 @@ tg_post_build() {
 build_kernel() {
 	if [ $INCREMENTAL = 0 ]
 	then
+		msg "|| Cleaning Sources ||"
 		make clean && make mrproper && rm -rf out
 	fi
 
 	if [ "$PTTG" = 1 ]
  	then
-		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
-		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$GRPID"
-        fi
+		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
+	fi
+
 	make O=out $DEFCONFIG
 	if [ $DEF_REG = 1 ]
 	then
 		cp .config arch/arm64/configs/$DEFCONFIG
 		git add arch/arm64/configs/$DEFCONFIG
 		git commit -m "$DEFCONFIG: Regenerate
-This is an auto-generated commit"
+
+						This is an auto-generated commit"
 	fi
 
 	BUILD_START=$(date +"%s")
 	
 	if [ $COMPILER = "clang" ]
 	then
-	    make -j"$PROCS" O=out \
-		NM=llvm-nm \
-		OBJCOPY=llvm-objcopy \
-		LD=ld.lld \
-		CROSS_COMPILE=aarch64-linux-gnu- \
-		CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-		CC=clang \
-		AR=llvm-ar \
-		OBJDUMP=llvm-objdump \
-		STRIP=llvm-strip | tee error.log
-        fi
+		MAKE+=(
+			CROSS_COMPILE=aarch64-linux-gnu- \
+			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+			CC=clang \
+			AR=llvm-ar \
+			OBJDUMP=llvm-objdump \
+			STRIP=llvm-strip
+		)
+	fi
+	
+	if [ $SILENCE = "1" ]
+	then
+		MAKE+=( -s )
+	fi
+
+	msg "|| Started Compilation ||"
+	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
+	make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
+
 		BUILD_END=$(date +"%s")
 		DIFF=$((BUILD_END - BUILD_START))
 
-        if ! [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz ]; then
-	        echo -e "Kernel compilation failed, See buildlog to fix errors"
-	        tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
-                tg_post_build "error.log" "$GRPID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
-	        exit 1
-	fi
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ] 
+	    then
+	    	msg "|| Kernel successfully compiled ||"
+	    	if [ $BUILD_DTBO = 1 ]
+			then
+				msg "|| Building DTBO ||"
+				tg_post_msg "<code>Building DTBO..</code>" "$CHATID"
+				python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
+			fi
+				gen_zip
+		else
+			if [ "$PTTG" = 1 ]
+ 			then
+				tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+			fi
+		fi
 	
-	mkdir "$AK3"/kernel/
-	mv /root/project/ginkgo/out/arch/arm64/boot/Image.gz "$AK3"/kernel/Image.gz
-	mkdir $AK3/dtbs/
-        mv /root/project/ginkgo/out/arch/arm64/boot/dts/qcom/trinket.dtb "$AK3"/dtbs/
+}
+
+##--------------------------------------------------------------##
+
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
 	if [ $BUILD_DTBO = 1 ]
 	then
-		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img $AK3/dtbo.img
+		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
 	fi
-	cd "$AK3" || exit
-	zip -r9 $ZIPNAME-$DEVICE-$CI_BRANCH-"$DATE" * -x .git
+	cd AnyKernel3 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DATE" * -x .git README.md
 
 	## Prepare a final zip variable
-	ZIP_FINAL="$ZIPNAME-$DEVICE-$CI_BRANCH-$DATE.zip"
-
-	if [ $SIGN = 1 ]
-	then
-		## Sign the zip before sending it to telegram
-		if [ "$PTTG" = 1 ]
- 		then
- 			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>" "$CHATID"
-  			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>" "$GRPID"
-                fi
-		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-		java -jar zipsigner-3.0.jar $ZIPNAME-$DEVICE-$CI_BRANCH"$DATE".zip "$ZIP_FINAL"-signed.zip
-		ZIP_FINAL="$ZIP_FINAL-signed.zip"
-        fi
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DATE.zip"
 
 	if [ "$PTTG" = 1 ]
  	then
 		tg_post_build "$ZIP_FINAL" "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
-		tg_post_build "$ZIP_FINAL" "$GRPID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 	fi
 	cd ..
 }
@@ -281,5 +296,10 @@ This is an auto-generated commit"
 clone
 exports
 build_kernel
+
+if [ $LOG_DEBUG = "1" ]
+then
+	tg_post_build "error.log" "$CHATID" "Debug Mode Logs"
+fi
 
 ##----------------*****-----------------------------##
